4dc73acc45af9cb8b24e3836f49aa087738885d8
Claire Prin-Guénon <pringuenon.claire@gmail.com>
2025-06-10 14:35:52 +0200

    commit for evaluation final

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -4,6 +4,7 @@ import { fetchData } from "./call-api.js";
 export const API_BASE_URL = "http://localhost:5678/api";
 export const AUTH_TOKEN_KEY = "authToken";
 
+//.1 Submitting login form
 form?.addEventListener("submit", async (event) => {
   event.preventDefault();
 
@@ -28,51 +29,54 @@ form?.addEventListener("submit", async (event) => {
   }
 });
 
-// Login <-> Logout
+//2. Login <-> Logout
 export function displayAdmin() {
   const token = localStorage.getItem(AUTH_TOKEN_KEY);
   const portfolioHeader = document.querySelector(".portfolio__header");
   const filters = document.querySelector(".filters");
-
   const navList = document.querySelector("header nav ul");
-  let loginLogoutLi = navList.querySelector(".login-logout");
-  if (!loginLogoutLi) {
-    loginLogoutLi = document.createElement("li");
-    loginLogoutLi.className = "login-logout";
-    const instaLi = navList.querySelector("li:last-child");
-    navList.insertBefore(loginLogoutLi, instaLi);
-  }
+  const loginLogoutLi = navList.querySelector(".login-logout");
 
   if (token) {
-    const editionHeader = document.createElement("div");
-    editionHeader.className = "edition-mode";
-    editionHeader.innerHTML = `
-      <img src="assets/icons/edition-mode.svg" alt="mode edition" />
-      <p>Mode édition</p>
-    `;
-    document.body.insertBefore(editionHeader, document.body.firstChild);
-
-    const modifier = document.createElement("div");
-    modifier.className = "portfolio__edition";
-    modifier.innerHTML = `
-      <img src="assets/icons/edition-mode_black.svg" alt="mode edition" />
-      <a class="js-modal" href="#modal">Modifier</a>
-    `;
-    portfolioHeader.appendChild(modifier);
-
-    filters.style.display = "none";
-    portfolioHeader.classList.add("edition");
-
     loginLogoutLi.innerHTML = `<a href="#" id="logout-link">logout</a>`;
-    document.getElementById("logout-link").addEventListener("click", (e) => {
+    const logoutLink = document.getElementById("logout-link");
+    logoutLink.addEventListener("click", (e) => {
       e.preventDefault();
       localStorage.removeItem(AUTH_TOKEN_KEY);
       window.location.reload();
     });
   } else {
+    loginLogoutLi.innerHTML = `<a href="login.html">login</a>`;
+  }
+
+  // Display Admin <-> client
+  if (token) {
+    const editionHeader = document.createElement("div");
+    editionHeader.className = "edition-mode";
+    editionHeader.innerHTML = `
+        <img src="assets/icons/edition-mode.svg" alt="mode edition" />
+        <p>Mode édition</p>
+      `;
+    document.body.insertBefore(editionHeader, document.body.firstChild);
+
+    const modifier = document.createElement("div");
+    modifier.className = "portfolio__edition";
+    modifier.innerHTML = `
+        <img src="assets/icons/edition-mode_black.svg" alt="mode edition" />
+        <a class="js-modal" href="#modal">Modifier</a>
+      `;
+    portfolioHeader.appendChild(modifier);
+
+    filters.style.display = "none";
+    portfolioHeader.classList.add("edition");
+  } else {
+    const editionHeader = document.querySelector(".edition-mode");
+    editionHeader.remove();
+
+    const modifier = portfolioHeader.querySelector(".portfolio__edition");
+    modifier.remove();
+
     filters.style.display = "flex";
     portfolioHeader.classList.remove("edition");
-
-    loginLogoutLi.innerHTML = `<a href="login.html">login</a>`;
   }
 }
